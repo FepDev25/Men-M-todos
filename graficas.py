945865d0626afef341cbf71e2e23721d7acb03f2
Felipe <lamediatricolor@gmail.com>
2024-05-03 08:56:54 -0500

    Terminando Newton Raphson

--- graficas.py
+++ graficas.py
@@ -19,3 +19,21 @@ def graficar_cerrados(simbolo, mi_funcion, rango_x, rango_y, raiz):
     plt.grid(True)
     plt.legend()
     plt.show()
+
+def graficar_abiertos(simbolo, mi_funcion, rango, raiz):
+    x = sp.Symbol(simbolo)
+    funcion = mi_funcion
+
+    funcion_numpy = sp.lambdify(x, funcion, 'numpy')
+
+    x_vals = np.linspace(rango-10, rango+10, 100)
+    y_vals = funcion_numpy(x_vals)
+
+    plt.plot(x_vals, y_vals, label=f'$f(x) = {str(funcion)}')
+    plt.xlabel('x')
+    plt.ylabel('f(x)')
+    plt.title('Gráfico de $f(x)$')
+    plt.scatter(raiz, 0, color='red')
+    plt.grid(True)
+    plt.legend()
+    plt.show()
--- newton_raphson.py
+++ newton_raphson.py
@@ -6,8 +6,9 @@ import tkinter as tk
 from tabla import Table
 from scipy.optimize import root
 from tkinter import messagebox
+from graficas import graficar_abiertos
 
-def newton_raphson(funcion_x, limite_iteraciones, x_inicial, valor_error_aproximado, error_verdadero):
+def newton_raphson(funcion_x, limite_iteraciones, x_inicial, valor_error_aproximado, valor_error_verdadero):
     x_n_1 = 0
     x_n = x_inicial
     x = sp.Symbol('x')
@@ -15,58 +16,121 @@ def newton_raphson(funcion_x, limite_iteraciones, x_inicial, valor_error_aproxim
     f_prima = sp.diff(funcion, x)
     funcion_derivada = f_prima
     iteracion = 1
-    print(f"x{iteracion}: {x_n}")
-    print()
+
+    datos_iteraciones = []
+
+    funcion_numerica = sp.lambdify(x, funcion)
+    solucion = root(funcion_numerica, x_inicial) 
+    valor_verdadero = solucion.x[0]
 
     while iteracion <= limite_iteraciones:
-        iteracion += 1
         x_n_1 = x_n
 
         f_xn = funcion.subs(x, x_n)
         f_dxn = funcion_derivada.subs(x, x_n)
-        print(f"{x_n} evaluado en la funcion: {f_xn.evalf()}")
-        print(f"{x_n} evaluado en la derivada: {f_dxn.evalf()}")
 
         x_n = (x_n - (f_xn/f_dxn)).evalf()
 
         error_aproximado = x_n - x_n_1
         error_aproximado_porcentual = (error_aproximado / x_n) * 100
-        print(f"x{iteracion}: {x_n}")
-        print(f"Error aproximado porcentual: {abs(error_aproximado_porcentual)}")
 
+        error_verdadero =  x_n - valor_verdadero
+        error_verdadero_porcentual = (error_verdadero / valor_verdadero) * 100
+
+        cifras_redondeo = 12
+        datos_iteraciones.append({
+            'Xi': round(x_n_1, cifras_redondeo),
+            'Xi+1': round(x_n, cifras_redondeo),
+            'valor_verdadero': valor_verdadero,
+            'error_verdadero': round(abs(error_verdadero),cifras_redondeo),
+            'error_verdadero_porcentual': round(abs(error_verdadero_porcentual), cifras_redondeo), 
+            'error_aproximado':round( abs(error_aproximado), cifras_redondeo),
+            'error_aproximado_prcentual': round(abs(error_aproximado_porcentual), cifras_redondeo)
+        })
+
+        if (abs(error_verdadero_porcentual) <= valor_error_verdadero):
+            messagebox.showinfo("Raiz", f"Error verdadero alcanzado: {abs(error_verdadero_porcentual)}\nRaiz: {x_n}")
+            return x_n, iteracion, datos_iteraciones
         if (abs(error_aproximado_porcentual) <= valor_error_aproximado):
-            print(F"Error de {valor_error_aproximado}% alcanzado.")
-            return x_n
+            messagebox.showinfo("Raiz", f"Error aproximado alcanzado: {abs(error_aproximado_porcentual)}\nRaiz: {x_n}")
+            return x_n, iteracion, datos_iteraciones
         
-        print()
-    return x_n
-
-def graficar_newton_raphson(simbolo, mi_funcion, rango, raiz):
-    x = sp.Symbol(simbolo)
-    funcion = mi_funcion
-
-    funcion_numpy = sp.lambdify(x, funcion, 'numpy')
-
-    x_vals = np.linspace(rango-10, rango+10, 100)
-    y_vals = funcion_numpy(x_vals)
-
-    plt.plot(x_vals, y_vals, label=f'$f(x) = {str(funcion)}')
-    plt.xlabel('x')
-    plt.ylabel('f(x)')
-    plt.title('Gráfico de $f(x)$')
-    plt.scatter(raiz, 0, color='red')
-    plt.grid(True)
-    plt.legend()
-    plt.show()
+        iteracion += 1
+    messagebox.showinfo("Raiz", f"Iteraciones realizadas: {limite_iteraciones}\nRaiz: {x_n}")
+    return x_n, limite_iteraciones, datos_iteraciones
 
 def newton_raphsonn_method_window(root):
-    pass
+    window = tk.Toplevel(root)
+    window.title("Datos para Método de Newton Raphson")
 
-def calcular_newton_raphsonn(root):
-    pass
+    label_funcion = tk.Label(window, text="Ingrese la función en términos de x: ")
+    label_funcion.grid(row=0, column=0)
+    entrada_funcion = tk.Entry(window)
+    entrada_funcion.grid(row=0, column=1)
 
-"""valor_inicial = 9.5
-x = sp.Symbol('x')
-funcion = ((0.35 * math.e**(0.75*x)) / x) - 50
-raiz = newton_raphson(funcion, 10, valor_inicial, 0.00006)
-print(f"La raiz es {raiz}")"""
+    label_xn = tk.Label(window, text="Ingrese el valor para X inicial: ")
+    label_xn.grid(row=1, column=0)
+    entrada_xn = tk.Entry(window)
+    entrada_xn.grid(row=1, column=1)
+
+    label_iteraciones = tk.Label(window, text="Ingrese el número máximo de iteraciones: ")
+    label_iteraciones.grid(row=2, column=0)
+    entrada_iteraciones = tk.Entry(window)
+    entrada_iteraciones.grid(row=2, column=1)
+
+    label_error_aprox = tk.Label(window, text="Ingrese el porcentaje de error aproximado que desea manejar: ")
+    label_error_aprox.grid(row=3, column=0)
+    entrada_error_aprox = tk.Entry(window)
+    entrada_error_aprox.grid(row=3, column=1)
+
+    label_error_verdadero = tk.Label(window, text="Ingrese el porcentaje de error verdadero que desea manejar: ")
+    label_error_verdadero.grid(row=4, column=0)
+    entrada_error_verdadero = tk.Entry(window)
+    entrada_error_verdadero.grid(row=4, column=1)
+
+    boton_calcular = tk.Button(window, text="Calcular", command=lambda: calcular_newton_raphsonn(
+        entrada_funcion.get(), entrada_xn.get(), entrada_iteraciones.get(), entrada_error_aprox.get(), entrada_error_verdadero.get(), root, window))
+    boton_calcular.grid(row=5, columnspan=2, pady=5)
+
+def calcular_newton_raphsonn(funcion, x_inicial, iteraciones_max, error_prox_max, error_verd_max, root, window):
+    if error_prox_max == "":
+        error_prox_max = 0
+    else:
+        error_prox_max = float(error_prox_max)
+
+    if error_verd_max == "":
+        error_verd_max = 0
+    else:
+        error_verd_max = float(error_verd_max)
+
+    xn = float(x_inicial)
+    iteraciones = int(iteraciones_max)
+
+    funcion_expr = sp.sympify(funcion)
+
+    raiz, pasadas, datos_iteraciones = newton_raphson(funcion_expr, iteraciones, xn, error_prox_max, error_verd_max)
+
+    table_window = tk.Toplevel(root)
+    table_window.title("Tabla de Resultados")
+
+    table = Table(table_window, filas=pasadas+1, columnas=7)
+    table.pack(expand=True, fill=tk.BOTH)
+    table.set_cell_value(0, 0, "Xi")
+    table.set_cell_value(0, 1, "Xi+1")
+    table.set_cell_value(0, 2, "V Verd")
+    table.set_cell_value(0, 3, "Err V")
+    table.set_cell_value(0, 4, "Err V%")
+    table.set_cell_value(0, 5, "Err Apr")
+    table.set_cell_value(0, 6, "Err Apr%")
+
+    for i, datos in enumerate(datos_iteraciones, start=1): 
+        table.set_cell_value(i, 0, datos['Xi'])
+        table.set_cell_value(i, 1, datos['Xi+1'])
+        table.set_cell_value(i, 2, datos['valor_verdadero'])
+        table.set_cell_value(i, 3, datos['error_verdadero'])
+        table.set_cell_value(i, 4, datos['error_verdadero_porcentual'])
+        table.set_cell_value(i, 5, datos['error_aproximado'])
+        table.set_cell_value(i, 6, datos['error_aproximado_prcentual'])
+    
+    graficar_abiertos('x', funcion_expr, int(x_inicial), raiz)
+    
